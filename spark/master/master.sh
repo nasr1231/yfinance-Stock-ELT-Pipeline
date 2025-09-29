973d98892bae8c5af8f7b70e39376cc2a61151ae
Mohamed Nasr <81034955+nasr1231@users.noreply.github.com>
2025-09-29 19:38:22 +0300

    - Replaced spark base images for workers and master node with bitnami Image.
    - UPDATE: docker compose file with new configs for Spark master and worker.
    
    - ADD: added docker operator to the base docker image of Airflow Service.

--- spark/master/master.sh
+++ spark/master/master.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 set -e
+export SPARK_HOME=/opt/bitnami/spark
 
 echo "Starting Spark Master on port ${SPARK_MASTER_PORT} ..."
-$SPARK_HOME/sbin/start-master.sh --host 0.0.0.0 --port ${SPARK_MASTER_PORT} --webui-port ${SPARK_MASTER_WEBUI_PORT}
+$SPARK_HOME/sbin/start-master.sh --host 0.0.0.0 --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
 
 tail -f $SPARK_HOME/logs/*
